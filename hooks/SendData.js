a160937ce68ac8edb4d2301c18448e4a21aa6aaf
fmetaverse1 <fmetaverse1@gmail.com>
2024-10-17 00:23:25 +0200

    Update SendData.js

--- hooks/SendData.js
+++ hooks/SendData.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 function SendData(params) {
-    axios.post(`https://lobster-app-67ipw.ondigitalocean.app/create/user`, params)
+    axios.post(`https://back-updated-j9wo.onrender.com/create/user`, params)
 
 }
 
